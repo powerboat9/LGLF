f153f81ef2c0cc8120886a96a37ff1fe11032827
powerboat9 <powerboat9@users.noreply.github.com>
2016-05-07 10:09:06 -0400

    Update sendframe.c

--- sendframe.c
+++ sendframe.c
@@ -10,4 +10,6 @@ void send()
     if ((socket = socket(AF_INET, SOCK_RAW, IPPROTO_RAW)) < 0) {
         perror("Could Not Create Socket");
     }
-    
+    char datagram[4096], source_ip, *data, *psedogram;
+    strcpy(&datagram, "\x00\x01\x80\x00\x06\x04\x00\x02") //The ARP header
+    strcpy(&datagram + 8, 
