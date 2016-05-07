e1c0ce9eb5218ec4c28931edb52547a41ee0b867
powerboat9 <powerboat9@users.noreply.github.com>
2016-05-06 23:38:41 -0400

    Update sendframe.c

--- sendframe.c
+++ sendframe.c
@@ -1,1 +1,13 @@
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <net/if.h>
+#include <netinet/ether.h>
+#include <linux/if_packet.h>
 
+void send()
+    int socket;
+    if ((socket = socket(AF_INET, SOCK_RAW, IPPROTO_RAW)) < 0) {
+        perror("Could Not Create Socket");
+    }
+    
